017965fcd2e58ff380066d7b61588fb2ed142060
Kerry Su <i_sukai@live.com>
2017-01-04 17:33:04 +0800

    Add render registry.

--- src/main/java/net/sshockwave/tinycreatures/proxy/ClientProxy.java
+++ src/main/java/net/sshockwave/tinycreatures/proxy/ClientProxy.java
@@ -1,7 +1,16 @@
 package net.sshockwave.tinycreatures.proxy;
 
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.sshockwave.tinycreatures.init.Registry;
+
 /**
  * @author sshockwave
  */
 public class ClientProxy extends CommonProxy {
+
+    @Override
+    public void preInit(FMLPreInitializationEvent event){
+        Registry.registerRenders();
+    }
+
 }
