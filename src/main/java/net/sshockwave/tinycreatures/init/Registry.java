56c8669b336944ee73f05f29ce83dc5ccbb81dca
Kerry Su <i_sukai@live.com>
2017-01-05 16:56:42 +0800

    Add capabilities registry.

--- src/main/java/net/sshockwave/tinycreatures/init/Registry.java
+++ src/main/java/net/sshockwave/tinycreatures/init/Registry.java
@@ -1,7 +1,5 @@
 package net.sshockwave.tinycreatures.init;
 
-import static net.minecraftforge.fml.common.registry.GameRegistry.*;
-
 import net.minecraft.block.Block;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
@@ -9,8 +7,8 @@ import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.common.registry.GameData;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
-import net.sshockwave.tinycreatures.init.*;
 
+import static net.minecraftforge.fml.common.registry.GameRegistry.*;
 /**
  * @author sshockwave
  */
@@ -56,22 +54,24 @@ public class Registry {
     public static void registerRenders(){
 
     }
+
+    public static void registerCapabilities() {
+
+    }
 }
 
 class RegistryHelper{
 
     @SuppressWarnings("deprecation")
     @SideOnly(Side.CLIENT)
-    public static void registerRender(Item item)
-    {
+    public static void registerRender(Item item) {
         String name = GameData.getItemRegistry().getNameForObject(item).toString();
         ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation(name, "inventory"));
     }
 
     @SuppressWarnings("deprecation")
     @SideOnly(Side.CLIENT)
-    public static void registerRender(Block block)
-    {
+    public static void registerRender(Block block) {
         Item item = Item.getItemFromBlock(block);
         String name = GameData.getBlockRegistry().getNameForObject(block).toString();
         ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation(name, "inventory"));
